f6694ee0280e14b9e6aebadcde95e5105c359a9a
Muhammad Wildan Khalilurrhman <wildankhairusunny@gmail.com>
2023-05-29 21:59:57 +0700

    Perbaikan pada GDocs

--- GDocs/Docs.go
+++ GDocs/Docs.go
@@ -1,5 +1,13 @@
 package gdocs
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/api/docs/v1"
+)
+
 func BuatFile(namaFile string) error {
 	file, err := os.Create(namaFile)
 	if err != nil {
@@ -18,61 +26,62 @@ func BuatDirektori(namaDirektori string) error {
 	if err != nil {
 		return fmt.Errorf("gagal membuat direktori: %v", err)
 	}
+	return nil
+}
 
-	func DeleteDocument(documentID, credentialsFile string) error {
-		ctx := context.Background()
-	
-		srv, err := docs.NewService(ctx, option.WithCredentialsFile(credentialsFile))
-		if err != nil {
-			return fmt.Errorf("gagal membuat layanan Google Docs: %v", err)
-		}
-	
-		req := docs.BatchUpdateDocumentRequest{
-			Requests: []*docs.Request{
-				{
-					DeleteContentRange: &docs.DeleteContentRangeRequest{
-						Range: &docs.Range{
-							StartIndex: 1, // Menghapus semua isi dokumen
-						},
+func DeleteDocument(srv *docs.Service,documentID, credentialsFile string) error {
+	// ctx := context.Background()
+
+	// srv, err := docs.NewService(ctx, option.WithCredentialsFile(credentialsFile))
+	// if err != nil {
+	// 	return fmt.Errorf("gagal membuat layanan Google Docs: %v", err)
+	// }
+
+	req := docs.BatchUpdateDocumentRequest{
+		Requests: []*docs.Request{
+			{
+				DeleteContentRange: &docs.DeleteContentRangeRequest{
+					Range: &docs.Range{
+						StartIndex: 1, // Menghapus semua isi dokumen
 					},
 				},
 			},
-		}
-	
-		_, err = srv.Documents.BatchUpdate(documentID, &req).Do()
-		if err != nil {
-			return fmt.Errorf("gagal menghapus dokumen: %v", err)
-		}
-	
-		return nil
+		},
 	}
 
-	func Filebaru() {
-		ctx := context.Background()
-		b, err := os.ReadFile("credentials.json")
-		if err != nil {
-			log.Fatalf("Unable to read client secret file: %v", err)
-		}
-	
-		// If modifying these scopes, delete your previously saved token.json.
-		config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents.readonly")
-		if err != nil {
-			log.Fatalf("Unable to parse client secret file to config: %v", err)
-		}
-		client := client.GetClient(config)
-	
-		srv, err := docs.NewService(ctx, option.WithHTTPClient(client))
-		if err != nil {
-			log.Fatalf("Unable to retrieve Docs client: %v", err)
-		}
-	
-		// Prints the title of the requested doc:
-		// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
-		docId := "1WZYmZk_YSU0TAQwpHrXMba-ZmfeQtENEZ-ykZ4WQZTY"
-		doc, err := srv.Documents.Get(docId).Do()
-		if err != nil {
-			log.Fatalf("Unable to retrieve data from document: %v", err)
-		}
-		fmt.Printf("The title of the doc is: %s\n", doc.Title)
+	_,err := srv.Documents.BatchUpdate(documentID, &req).Do()
+	if err != nil {
+		return fmt.Errorf("gagal menghapus dokumen: %v", err)
 	}
 
+	return nil
+}
+
+func Filebaru(srv *docs.Service, docsID string) {
+	// ctx := context.Background()
+	// b, err := os.ReadFile("credentials.json")
+	// if err != nil {
+	// 	log.Fatalf("Unable to read client secret file: %v", err)
+	// }
+
+	// // If modifying these scopes, delete your previously saved token.json.
+	// config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents.readonly")
+	// if err != nil {
+	// 	log.Fatalf("Unable to parse client secret file to config: %v", err)
+	// }
+	// client := client.GetClient(config)
+
+	// srv, err := docs.NewService(ctx, option.WithHTTPClient(client))
+	// if err != nil {
+	// 	log.Fatalf("Unable to retrieve Docs client: %v", err)
+	// }
+
+	// Prints the title of the requested doc:
+	// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+	docId := docsID
+	doc, err := srv.Documents.Get(docId).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from document: %v", err)
+	}
+	fmt.Printf("The title of the doc is: %s\n", doc.Title)
+}
