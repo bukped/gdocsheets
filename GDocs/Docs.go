d4539bfe22fee23560499d390cded21ea2bd61ed
Pisang <114119615+EtoniaThea@users.noreply.github.com>
2023-05-29 21:31:39 +0700

    Add files via upload

---
+++ GDocs/Docs.go
@@ -0,0 +1,78 @@
+package gdocs
+
+func BuatFile(namaFile string) error {
+	file, err := os.Create(namaFile)
+	if err != nil {
+		return fmt.Errorf("gagal membuat file: %v", err)
+	}
+	defer file.Close()
+
+	// Lakukan operasi lain terkait pembuatan file
+
+	return nil
+}
+
+// BuatDirektori adalah fungsi untuk membuat direktori baru
+func BuatDirektori(namaDirektori string) error {
+	err := os.Mkdir(namaDirektori, 0755)
+	if err != nil {
+		return fmt.Errorf("gagal membuat direktori: %v", err)
+	}
+
+	func DeleteDocument(documentID, credentialsFile string) error {
+		ctx := context.Background()
+	
+		srv, err := docs.NewService(ctx, option.WithCredentialsFile(credentialsFile))
+		if err != nil {
+			return fmt.Errorf("gagal membuat layanan Google Docs: %v", err)
+		}
+	
+		req := docs.BatchUpdateDocumentRequest{
+			Requests: []*docs.Request{
+				{
+					DeleteContentRange: &docs.DeleteContentRangeRequest{
+						Range: &docs.Range{
+							StartIndex: 1, // Menghapus semua isi dokumen
+						},
+					},
+				},
+			},
+		}
+	
+		_, err = srv.Documents.BatchUpdate(documentID, &req).Do()
+		if err != nil {
+			return fmt.Errorf("gagal menghapus dokumen: %v", err)
+		}
+	
+		return nil
+	}
+
+	func Filebaru() {
+		ctx := context.Background()
+		b, err := os.ReadFile("credentials.json")
+		if err != nil {
+			log.Fatalf("Unable to read client secret file: %v", err)
+		}
+	
+		// If modifying these scopes, delete your previously saved token.json.
+		config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents.readonly")
+		if err != nil {
+			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		}
+		client := client.GetClient(config)
+	
+		srv, err := docs.NewService(ctx, option.WithHTTPClient(client))
+		if err != nil {
+			log.Fatalf("Unable to retrieve Docs client: %v", err)
+		}
+	
+		// Prints the title of the requested doc:
+		// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+		docId := "1WZYmZk_YSU0TAQwpHrXMba-ZmfeQtENEZ-ykZ4WQZTY"
+		doc, err := srv.Documents.Get(docId).Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve data from document: %v", err)
+		}
+		fmt.Printf("The title of the doc is: %s\n", doc.Title)
+	}
+
